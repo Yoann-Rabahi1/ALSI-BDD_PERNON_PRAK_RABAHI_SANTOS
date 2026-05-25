ed2f665094c331af17c369eba5f82b207f62b384
Yoann <rabahiyoann@gmail.com>
2026-05-25 15:22:03 +0200

    definissions de l'engine et de la session local ainsi que la class base pour interagir avec la bdd

--- backend/database.py
+++ backend/database.py
@@ -1,20 +1,31 @@
-import mysql.connector
 import os
-from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# Chargement des variables d'environnement (.env)
-load_dotenv()
+host=os.getenv("DB_HOST", "localhost")
+user=os.getenv("DB_USER", "root"),
+password=os.getenv("DB_PASSWORD", "")
+database=os.getenv("DB_NAME", "clinique_veto")
 
-def get_db_connection():
-    """Crée une connexion à la base de données MySQL."""
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:tmQLJzXIetdYXDhdibFMpPROnJldXqWB@kodama.proxy.rlwy.net:11053/railway"
+
+# Création de l'engine
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+# Création de la fabrique de sessions
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# La classe de base pour tes modèles
+Base = declarative_base()
+
+# Fonction utilitaire pour récupérer une session de BDD (Dependency Injection)
+def get_db():
+    db = SessionLocal()
     try:
-        connection = mysql.connector.connect(
-            host=os.getenv("DB_HOST", "localhost"),
-            user=os.getenv("DB_USER", "root"),
-            password=os.getenv("DB_PASSWORD", ""),
-            database=os.getenv("DB_NAME", "clinique_veto")
-        )
-        return connection
-    except mysql.connector.Error as err:
-        print(f"Erreur de connexion : {err}")
-        return None
+        yield db
+    finally:
+        db.close()
+
