445e1b991e5dcddf2d6720ba646842f86ef71b91
Yoann <rabahiyoann@gmail.com>
2026-05-25 15:22:28 +0200

    premiere route fonctionnelle pour créer un user

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends, status
+from pydantic import BaseModel
+from typing import Annotated
+import models
 from fastapi.middleware.cors import CORSMiddleware
-from database import get_db_connection
+from database import engine, SessionLocal, get_db
+from sqlalchemy.orm import Session
+from schema import *
 
 app = FastAPI(title="API Clinique Vétérinaire - ALSI61")
+models.Base.metadata.create_all(bind=engine)
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -11,6 +18,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def read_root():
-    return {"message": "Bienvenue sur l'API de gestion de la clinique"}
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.post("/users/", response_model=CompteUser)
+async def create_user(user: CompteUserCreate, db:db_dependency):
+    db_user = models.CompteUser(**user.model_dump())
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
